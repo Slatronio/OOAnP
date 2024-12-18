2921cdd2a132a2d40f57f242af9a8fc62a9d6062
Slatronio <slavyankokorin@gmail.com>
2024-12-18 13:47:01 +0600

    CollisionTreeBuilder

---
+++ spacebattle/SpaceBattle.Lib/CollisionTreeBuilder.cs
@@ -0,0 +1,30 @@
+﻿using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+public interface ITrieBuilder
+{
+    public void BuildFromFile(string path);
+}
+
+public class TrieBuilder : ITrieBuilder
+{
+    private static IEnumerable<IEnumerable<int>> ReadFileData(string path)
+    {
+        return File.ReadAllLines(path).Select(line => line.Split().Select(int.Parse));
+    }
+
+    public void BuildFromFile(string path)
+    {
+        ReadFileData(path).ToList().ForEach(vector =>
+        {
+            var prefixTrie = IoC.Resolve<IDictionary<int, object>>("Game.CollisionTree");
+
+            vector.ToList().ForEach(feature =>
+            {
+                prefixTrie.TryAdd(feature, new Dictionary<int, object>());
+                prefixTrie = (IDictionary<int, object>)prefixTrie[feature];
+            });
+        });
+    }
+}
